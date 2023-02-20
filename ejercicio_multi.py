f7b346cc5eb60aa62f9ccab7777424c682bd6092
miguelig <isazamiguel123@gmail.com>
2023-02-20 07:44:54 -0500

    tipos_variables

---
+++ ejercicio_multi.py
@@ -0,0 +1,8 @@
+number_one = 33
+number_two = 15
+
+
+
+multiplicar= number_one * number_two
+
+print(multiplicar)
---
+++ example_one.py
@@ -0,0 +1,31 @@
+"""
+name= "Jesus maria gomez"
+print("Hello", name)
+
+# ejemplo de casting (convertir una variable de un tipo de dato a otro dato)
+variable=5
+variable_uno = float(variable)
+variable_dos = str(variable)
+
+
+#print(variable, variable_uno)
+print(variable, variable_uno,  variable_dos)
+print(type(variable_dos))
+print(type(variable_uno))
+print(type(variable))
+#print(suma_datos)
+"""
+
+#tipos de datos
+variable1 = "Hola" #str (string)
+variable2 = 20 #int (entero)
+variable3 = 20.8 #float (decimal)
+variable4 = 1j #complex
+variable7 = True #bool (logico)
+variable5 = ["cantar", "jugar", "estudair"] #list
+variable6 = ("cantar", "jugar", "estudair") #tupla
+carro = {"color": "rojo", "velocidad": 300, "modelo": "GTRR36"} #dict
+#print(variable5)
+#print(variable6)
+#print(carro)
+print(variable7)

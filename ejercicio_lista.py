5cd88e8df17c4ad704bfcc68d2ac24047767dbeb
miguelig <isazamiguel123@gmail.com>
2023-02-22 09:00:35 -0500

    actividad

---
+++ ejercicio_lista.py
@@ -0,0 +1,25 @@
+#exampls of list
+#nombres = ["Jesus", "Maria", "Jose"] 
+#print(nombres[1])
+
+#datos_persona = ["Jesus", 33, 1.75, True]
+#en formato Diccionario
+"""
+datos_persona = {
+    "nombre":"Jesus",
+     "edad":33,
+      "altura":1.75,
+      "soltero?":True
+     }
+"""
+
+#en forma de tupla
+datos_persona = ["Jesus", 33, 1.75, True]
+#print(datos_persona[2])
+
+price = [100, 250, 300]
+resultado = price[0] + price[2]
+print("El cliente", datos_persona[0], "compro $", resultado)
+print(price)
+
+
